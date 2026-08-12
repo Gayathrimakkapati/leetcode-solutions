9260950d558745e53f83eadca635e01499ca723a
Gayathri Makkapati <lgayathrimakkapati@gmail.com>
2026-08-12 22:52:25 +0530

    Time: 0 ms (100%), Space: 19.3 MB (55.73%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,3 +1,12 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        
+        num_str=""
+        for i in range(len(digits)):
+            num_str=num_str+str(digits[i])
+        num_int=int(num_str)
+        num_int+=1
+        new_str=str(num_int)
+        res=[]
+        for digits in new_str:
+            res.append(int(digits))
+        return res
